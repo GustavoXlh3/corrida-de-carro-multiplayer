fd006cd7c80697e051643621b57567b3397cdc49
marip009 <mariana.silva@solucx.com.br>
2022-05-06 19:15:55 -0300

    docs: Added sprites

--- js/Game.js
+++ js/Game.js
@@ -6,6 +6,17 @@ class Game {
     form.display();
     player = new Player();
     playerCount = player.getPlayerCount();
+
+    car1 = createSprite(width/2-100, height-50);
+    car1.addImage(car1Img);
+    car1.scale = 0.07;
+    
+    car2 = createSprite(width/2+100, height-50);
+    car2.addImage(car2Img);
+    car2.scale = 0.07;
+    
+    //array
+    cars = [car1, car2];
   }
   getState(){
     var gameRef = database.ref("gameState");
@@ -29,5 +40,10 @@ class Game {
   play() {
     Player.getInfosPlayer();
     this.handleElements();
+
+    if(players != undefined) {
+      image(trackImg, 0, -height * 5, width, height * 6);
+    }
+    drawSprites();
   }
 }
--- sketch.js
+++ sketch.js
@@ -6,9 +6,14 @@ var form, player;
 var playerCount = 0;
 var gameState;
 var players;
+var trackImg;
+var cars, car1, car2, car1Img, car2Img;
 
 function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
+  trackImg = loadImage('./assets/track.jpg');
+  car1Img = loadImage('./assets/car1.png');
+  car2Img = loadImage('./assets/car2.png');
 }
 
 function setup() {
