cad8214c02269bd073cefe3dc9678bfb107e23ff
marip009 <mariana.silva@solucx.com.br>
2022-05-13 19:17:23 -0300

    docs: Alterações da leaderboard

--- js/Game.js
+++ js/Game.js
@@ -2,6 +2,9 @@ class Game {
   constructor() {
     this.resetButton = createButton("");
     this.resetTitle = createElement("h2");
+    this.leaderTitle = createElement("h2");
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
   }
 
   start() {
@@ -43,6 +46,17 @@ class Game {
     this.resetTitle.position(width/2+350, 40);
     this.resetTitle.html("reiniciar jogo");
     this.resetTitle.class("resetText");
+
+    this.leaderTitle.position(width / 18, 40);
+    this.leaderTitle.html("placar");
+    this.leaderTitle.class("resetText");
+
+    this.leader1.position(width / 18, 80);
+    this.leader1.html("leader1");
+    this.leader1.class("leadersText");
+    this.leader2.position(width / 18, 130);
+    this.leader2.html("leader2");
+    this.leader2.class("leadersText");
   }
 
   handleResetButton() {
@@ -64,6 +78,7 @@ class Game {
     if(players != undefined) {
       image(trackImg, 0, -height * 5, width, height * 6);
       var index = 0;
+      this.showLeaderboard();
       for (var plr in players){
         var y = height - players[plr].positionY;
         var x = players[plr].positionX;
@@ -75,7 +90,7 @@ class Game {
           fill('red');
           ellipse(x, y, 60, 60);
           camera.position.x = width/2;
-
+          
           if(players[plr].positionY > height) {
             camera.position.y = y;
           }
@@ -103,5 +118,40 @@ class Game {
       player.positionX -= 10;
       player.update();
     }
+  }
+
+  showLeaderboard() {
+    var leader1, leader2;
+    /* 
+      {
+        player1: {
+          name: "Gustavo",
+          score: 12
+        } 
+      }
+    */
+    var player = Object.values(players);
+    if (
+      (player[0].rank === 0 && player[1].rank === 0) ||
+      player[0].rank === 1
+    ) {
+      // &emsp;    Essa etiqueta é usada para exibir quatro espaços.
+      leader1 =
+        player[0].rank + "&emsp;" + player[0].name + "&emsp;" + player[0].score;
+
+      leader2 =
+        player[1].rank + "&emsp;" + player[1].name + "&emsp;" + player[1].score;
+    }
+
+    if (player[1].rank === 1) {
+      leader1 =
+        player[1].rank + "&emsp;" + player[1].name + "&emsp;" + player[1].score;
+
+      leader2 =
+        player[0].rank + "&emsp;" + player[0].name + "&emsp;" + player[0].score;
+    }
+
+    this.leader1.html(leader1);
+    this.leader2.html(leader2);
   }
 }
